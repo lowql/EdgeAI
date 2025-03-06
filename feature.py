c6be4b2f9e860b6c1f780830254991b5c24c0f26
vemi <kolagh@gmail.com>
2025-03-06 20:30:13 +0800

    Refactoring:: Strategy Pattern + Decorator Pattern

--- feature.py
+++ feature.py
@@ -2,68 +2,37 @@ from scipy.signal import butter, lfilter
 import numpy as np
 import matplotlib.pyplot as plt
 from  pandas.core.frame import DataFrame
-
 def rolling_window(feature:DataFrame,window_size=10):
-    return [feature[i:i+window_size].tolist() for i in range(len(feature)-window_size)]
-        
-class Transform:
-    def __init__(self,time_signal):
-        self.time_signal = time_signal
-        self.idx = len(time_signal)
-        self.idx_half = self.idx // 2
-    def time_to_freq(self,time_step=0.01):
+    return [feature[i:i+window_size].tolist() for i in range(len(feature)-window_size+1)]
+
+class SignalProcessor:
+    def process(self,signal,**kwargs):
+        raise NotImplementedError
+
+class MinMaxProcessor(SignalProcessor):
+    def min_max(self,x,axis = None):
+        x = np.asarray(x)
+        min = x.min(axis=axis,keepdims=True)
+        max = x.max(axis=axis,keepdims=True)
+        return (x-min)/(max-min)
+    def process(self, signal, **kwargs):
+        return self.min_max(signal,**kwargs)
+class FFTProcessor(SignalProcessor):
+    def time_to_freq(self,time_signal,time_step=0.01):
         """
             time_step=0.001: 假設取樣率為 100 Hz
         """
-        # 計算 FFT
-        self.freq_signal = np.fft.fft(self.time_signal)
-        
-        self.freqs = np.fft.fftfreq(self.idx,d=time_step)  
-        self.freqs_half = self.freqs[:self.idx_half]
-       
-        self.magnitude_half = np.abs(self.freq_signal[:self.idx_half])
-        self.magnitude = np.abs(self.freq_signal)
-    def freq_pass_butterfly(self):
-        porcessor = SignalProcess()
-        self.time_signal = porcessor.butter_bandpass_filter(self.time_signal,lowcut=10,highcut=50,fs=200)
-        return self.time_signal
-    def plot(self,figsize=(10,6)):
-        fig, ax = plt.subplots(4, 1, figsize=figsize)
-        # 時域信號
-        self.time_to_freq()
-        ax[0].plot(self.time_signal, label="ECG Signal")
-        ax[0].set_title("Time Domain Signal")# 時域信號
-        ax[0].set_xlabel("time")# 時間點
-        ax[0].set_ylabel("Amplitude")# 振幅
-        ax[0].legend()
-        
-        # 頻域信號
-        ax[1].stem(self.freqs_half, self.magnitude_half, 'b',basefmt=" ")
-        ax[1].set_title("Frequency Domain Signal (FFT)")
-        ax[1].set_xlabel("Frequency (Hz)")
-        ax[1].set_ylabel("Magnitude")
-        
-        # 頻域信號
-        self.freq_pass_butterfly()
-        self.time_to_freq()
-        ax[2].plot(self.time_signal, label="ECG Signal")
-        ax[2].set_title("Filter Time Domain Signal")# 時域信號
-        ax[2].set_xlabel("time")# 時間點
-        ax[2].set_ylabel("Amplitude")# 振幅
-        ax[2].legend()
+        signal_len = len(time_signal)
+        idx,idx_half = signal_len,signal_len//2
+        freq_signal = np.fft.fft(time_signal)
+        freqs = np.fft.fftfreq(idx,d=time_step)  
+        freqs_half = freqs[:idx_half]
+        magnitude_half = np.abs(freq_signal[:idx_half])
+        return {"freqs":freqs_half,"magnitude":magnitude_half}
+    def process(self, signal, **kwargs):
+        return self.time_to_freq(signal,**kwargs)
 
-        
-        # 頻域信號
-        ax[3].stem(self.freqs_half, self.magnitude_half, 'b',basefmt=" ")
-        ax[3].set_title("Filter Frequency Domain Signal (FFT)")
-        ax[3].set_xlabel("Frequency (Hz)")
-        ax[3].set_ylabel("Magnitude")
-        plt.tight_layout()
-        plt.show()
-
-            
-
-class SignalProcess:
+class ButterBandpass(SignalProcessor):
     def butter_bandpass(self,lowcut:float, highcut:float, fs:int, order=5):
         """
         # from scipy.signal import butter, lfilter
@@ -91,6 +60,9 @@ class SignalProcess:
         b, a = self.butter_bandpass(lowcut, highcut, fs, order=order)
         y = lfilter(b, a, data)
         return y
+    
+    def process(self, signal, **kwargs):
+        return self.butter_bandpass_filter(signal,**kwargs)
     def signal_frequency_band_energies(self,sampled_signal, frequency_bands, sampling_frequency, order=5):
         energies = []
         for bands in frequency_bands:
@@ -102,7 +74,62 @@ class SignalProcess:
         energy = np.sum(filtered_signal**2) 
         return energy
 
-if __name__ == "__main__":
-    signal_processer = SignalProcess()
-    # ULF, LF, HF, UHF => [[0.01, 0.04], [0.04, 0.15], [0.15, 0.4], [0.4, 1.0]]
-    energies = signal_processer.signal_frequency_band_energies(mock_signal, [[0.01, 0.04], [0.04, 0.15], [0.15, 0.4], [0.4, 1.0]], 32)
+class SignalDecorator:
+    """裝飾器模式，允許對信號進行多重處理"""
+    def __init__(self, signal):
+        self.signal = signal
+        self.processors = []
+
+    def add_processor(self, processor:SignalProcessor, **kwargs):
+        """新增信號處理策略"""
+        self.processors.append((processor, kwargs))
+
+    def apply(self):
+        """依序應用處理策略"""
+        processed_signal = self.signal
+        results = {}
+        for processor, kwargs in self.processors:
+            result = processor.process(processed_signal, **kwargs)
+            if isinstance(result, dict):
+                results.update(result)  # 儲存結果
+            else:
+                processed_signal = result  # 更新處理後的信號
+        return processed_signal, results
+    
+class SignalVisualizer:
+    """負責繪圖"""
+    def __init__(self, original_signal, time_step=0.01):
+        self.original_signal = original_signal
+        self.time_step = time_step
+
+    def plot(self, processed_signal, results, figsize=(10,6)):
+        fig, ax = plt.subplots(3, 1, figsize=figsize)
+
+        # 原始信號
+        ax[0].plot(self.original_signal, label="Original Signal")
+        ax[0].set_title("Time Domain Signal")
+        ax[0].set_xlabel("Time")
+        ax[0].set_ylabel("Amplitude")
+        ax[0].legend()
+
+        # 頻域信號
+        if "freqs" in results and "magnitude" in results:
+            freqs_half = results["freqs"][:len(results["freqs"]) // 2]
+            magnitude_half = results["magnitude"][:len(results["magnitude"]) // 2]
+            ax[1].stem(freqs_half, magnitude_half, 'b', basefmt=" ")
+            ax[1].set_title("Frequency Domain Signal (FFT)")
+            ax[1].set_xlabel("Frequency (Hz)")
+            ax[1].set_ylabel("Magnitude")
+
+        # 處理後的信號
+        ax[2].plot(processed_signal, label="Processed Signal")
+        ax[2].set_title("Processed Time Domain Signal")
+        ax[2].set_xlabel("Time")
+        ax[2].set_ylabel("Amplitude")
+        ax[2].legend()
+
+        plt.tight_layout()
+        plt.show()
+            
+
+        
