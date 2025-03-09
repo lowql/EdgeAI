42112e573df3f435a2fb4fe7ba834f48c7588f1e
vemi <kolagh@gmail.com>
2025-03-09 20:53:27 +0800

    add: 【load】class Evaluate 評估效能

--- load.py
+++ load.py
@@ -2,6 +2,8 @@ import os,pickle
 import pandas as pd
 import nest_asyncio
 import asyncio
+import matplotlib.pyplot as plt
+import time
 from functools import partial
 from concurrent.futures import ProcessPoolExecutor,ThreadPoolExecutor
 nest_asyncio.apply() # IPython 環境中已經有 Event loop 需要做特殊處裡
@@ -107,11 +109,12 @@ class WESAD:
 
         feat_df = pd.DataFrame(features)
         return feat_df
-    def mutiT_feature_extraction(self,sample_n=4000,window_size=3000,cols=['ECG','EMG','label'],limit=0):
+    def mutiT_feature_extraction(self,sample_n=4000,window_size=3000,cols=['ECG','EMG','label'],limit=0,work_n=1):
         signal = self.group(sample_n=sample_n).loc[:,cols]
         rolling_windows = {key: self.rolling_window(signal[key],window_size=window_size) for key in cols}
-        signal_length = limit if limit > 0 else len(self.rolling_window(signal['ECG'], window_size=window_size))
-        print(f"signal length: {signal_length}")
+        max_len_of_signal = len(self.rolling_window(signal['ECG'], window_size=window_size))
+        signal_length = limit if limit > 0 else max_len_of_signal
+        print(f"signal length: {signal_length}/{max_len_of_signal}")
         
         process_window_func = partial(
             self._process_window,
@@ -119,13 +122,13 @@ class WESAD:
             cols = cols
         ) 
         features = []
-        with ThreadPoolExecutor() as exec:
+        print(f"multi thread use {work_n} works")
+        with ThreadPoolExecutor(max_workers=work_n) as exec:
             features = list(exec.map(process_window_func,range(signal_length)))
         feat_df = pd.DataFrame(features)
         return feat_df
     def _process_window(self,i,rolling_windows,cols):
         col_feature = {}  # 先存這一組 window 的特徵  
-        print("enter window")
         for key in cols:  
             time_signal = rolling_windows[key][i]  # 取出對應索引的窗口資料  
             decorator = feat.SignalDecorator(time_signal)  
@@ -144,9 +147,75 @@ class WESAD:
                 decorator.add_processor(feat.MeanProcessor())  
                 processed_signal, results = decorator.apply()  
                 col_feature[f"mean_{key}"] = processed_signal 
-        print(col_feature)
-        print('exit window')
         return col_feature
+
+class Evaluate:
+    plt.figure(figsize=(10, 6))
+    @classmethod
+    def with_row_limit(cls,wesad,limit_values):
+        # 存儲結果的字典
+        results = {
+            'limit': [],
+            'feature_extraction_time': [],
+            'mutiT_feature_extraction_time': []
+        }
+        
+        for limit in limit_values:
+            # 測量第一個函數的執行時間
+            start_time = time.time()
+            wesad.feature_extraction(limit=limit)
+            fe_time = time.time() - start_time
+            
+            # 測量第二個函數的執行時間
+            start_time = time.time()
+            wesad.mutiT_feature_extraction(limit=limit)
+            mutiT_time = time.time() - start_time
+            
+            # 儲存結果
+            results['limit'].append(limit)
+            results['feature_extraction_time'].append(fe_time)
+            results['mutiT_feature_extraction_time'].append(mutiT_time)
+            print(results)
+        
+        
+        cls.performance_data =  pd.DataFrame(results)
+        plt.plot(cls.performance_data['limit'], cls.performance_data['feature_extraction_time'], 'o-', label='feature_extraction'),
+        plt.plot(cls.performance_data['limit'], cls.performance_data['mutiT_feature_extraction_time'], 's-', label='mutiT_feature_extraction')
+    
+        return cls.performance_data
+    @classmethod
+    def with_diff_work(cls,wesad,work_n_s,limit=100):
+        results = {'time':[],'works':[]}
+        for work_n in work_n_s:
+            # 測量第二個函數的執行時間
+            start_time = time.time()
+            wesad.mutiT_feature_extraction(limit=limit,work_n=work_n)
+            mutiT_time = time.time() - start_time
+            results['works'].append(work_n)
+            results['time'].append(mutiT_time)
+        cls.performance_data =  pd.DataFrame(results)
+        plt.plot(cls.performance_data['works'], cls.performance_data['time'], 'o-', label='mutiT_feature_extraction with diff works'),
+        return cls.performance_data
+    @classmethod
+    def show(cls):
+        # 繪製性能比較圖
+        if cls.performance_data.empty:
+            raise ValueError("請先執行 with_XXX_XXX")
+            
+        plt.xlabel('limit')
+        plt.ylabel('calculate cost (s)')
+        plt.title('Comparison of performance')
+        plt.legend()
+        plt.grid(True)
+        # plt.xscale('log')  # 使用對數刻度以更好地顯示不同量級的 limit
+        plt.tight_layout()
+
+        # 顯示結果表格
+        print(cls.performance_data)
+
+        # 顯示圖表
+        plt.show()
+        
 # Use asyncio.run() in the correct context
 if __name__ == '__main__':
     wesad = WESAD()
